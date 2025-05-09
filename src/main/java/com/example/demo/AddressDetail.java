d9e19a5ae984d58d4116c243f868f50f1a82550a
aditiroy <raditi22@gmail.com>
2025-05-09 11:08:27 +0530

    Update AddressDetail.java

--- src/main/java/com/example/demo/AddressDetail.java
+++ src/main/java/com/example/demo/AddressDetail.java
@@ -1,5 +1,35 @@
 package com.example.demo;
 
-public class AddressDetail {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+public class AddressDetail {
+    String city;
+    String state;
+    String street;
+    String number;
+    String district;
+    String region;
+    String postcode;
+    String county;
+    double longitude;
+    double latitude;
+    String censusGeoCoderCity;
+    String censusGeoCoderState;
+    String censusGeoCoderZip;
+    String censusGeoCoderStreet;
+    double censusGeoCoderLongitude;
+    double censusGeoCoderLatitude;
+    String censusGeoCoderCounty;
+    String censusGeoCoderMunicipality;
+    String censusGeoCoderFromAddressNumber;
+    String censusGeoCoderToAddressNumber;
 }
