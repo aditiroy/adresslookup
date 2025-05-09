28a7b0dc9516a799b46d4b7bc309f317a091734c
aditiroy <raditi22@gmail.com>
2025-05-09 11:10:43 +0530

    Update RestClient.java

--- src/main/java/com/example/demo/RestClient.java
+++ src/main/java/com/example/demo/RestClient.java
@@ -1,5 +1,35 @@
 package com.example.demo;
 
-public class RestClient {
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Slf4j
+public class RestClient implements IRestClient {
+
+    @Override
+    public HttpResponse<String> get(URI uri) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response   = client.send(request, HttpResponse.BodyHandlers.ofString());
+            log.info("Status Code: {}" ,response.statusCode());
+            log.info("Response Body:{} ", response.body());
+        } catch (IOException | InterruptedException e) {
+           log.error("Error occurred: {}",e.getMessage());
+        }
+
+    return response;
+    }
+
 
 }
